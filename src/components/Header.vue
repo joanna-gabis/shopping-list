08c2c5bdd6ac34698a235984e8ec49da7039ebb8
joanna-gabis <joanna.gabis@gmail.com>
2018-08-14 17:03:52 +0200

    add toggle strikethrough on click

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,60 +1,82 @@
 <template>
-<div class="app">
- <div class='header'>
-    <h1>Shopping list</h1>
-  </div>
-
-  <div class="input">
-    <input
-      type='text'
-      placeholder='add item'
-      v-model='newItem'
-      @keyup.enter='saveItem'
-    />
-    <div
-      class='button'
-      @click='saveItem'>
-      <i class="fas fa-plus"></i>
+  <div class="app">
+    <div class='header'>
+      <h1>Shopping list</h1>
     </div>
-  </div>
 
-  <div class='list'>
-    <ul>
-      <li v-for='item in list'>{{ item }} <i class="fas fa-times"></i></li>
-    </ul>
-  </div>
+    <div class="input">
+      <input
+        type='text'
+        placeholder='add item'
+        v-model='newItem'
+        @keyup.enter='saveItem'
+      />
+      <div
+        class='button'
+        @click='saveItem'>
+        <i class="fas fa-plus"></i>
+      </div>
+    </div>
+    <div v-if='list.length===0'>Congratulations, you have nothing to buy!</div>
 
-</div>
+    <div class='list'>
+      <ul>
+        <li v-for='item in list' :class="{strikeout: item.purchased}" @click='togglePurchased(item)' >{{item.name}}</li>
+      </ul>
+    </div>
+
+  </div>
 </template>
 
 <script>
-export default {
-  name: 'App',
-  data() {
-    return {
-      newItem: '',
-      list: ['kasza', 'pomidory',],
-    };
-  },
-  methods: {
-    saveItem: function() {
-      this.list.push(this.newItem);
-      this.newItem = '';
+  export default {
+    name: 'App',
+    data() {
+      return {
+        newItem: '',
+        list: [],
+      };
     },
+    methods: {
+      saveItem: function() {
+        if(this.newItem) {
+          this.list.push({
+            name: this.newItem,
+            purchased: false,
+          });
+          this.newItem = '';
+        }
+      },
+      // deleteItem: function(item) {
+      //   let itemToDelete = this.list.find((elem) => elem.name===item.name);
+      //   let index = this.list.indexOf(itemToDelete);
+      //   this.list = this.list.slice(0, index).concat(this.list.slice(index+1));
+      // },
+      togglePurchased: function(item) {
+        item.purchased = !item.purchased;
+      }
+    }
   }
-}
 </script>
 
 <style scoped lang="scss">
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  .app {
+    width: 100%;
+  }
   h1 {
-    color: #5e5757;
+    color: #696969;
     margin: 50px;
   }
   input {
     border: none;
     border-bottom: 1px solid black;
     height: 50px;
-    width: 250px;
+    width: 200px;
     outline: none;
     font-size: 1.25rem;
     margin: 20px;
@@ -73,7 +95,19 @@ export default {
     color: #fff;
     font-size: 1.5rem;
   }
-  li {
-    list-style-type: none;
+  .list {
+    li {
+      list-style-type: none;
+      font-size: 1.5rem;
+
+      &:nth-child(odd) {
+        background-color: #DCDCDC;
+      }
+    }
+    .strikeout {
+      color: 	#696969;
+      text-decoration: line-through;
+    }
   }
+
 </style>
